ca3688b7c9cbe5c12086027300a1826882d1ff47
Charlie Sanders <sanderscharlie@gmail.com>
2021-12-15 23:27:43 -0600

    apply them too

--- external/storage/provision-and-populate-config-volumes.sh
+++ external/storage/provision-and-populate-config-volumes.sh
@@ -5,6 +5,6 @@ set -euxo pipefail
 apps="sabnzbd sonarr radarr"
 
 for app in $apps; do
-   kubectl -n $app -f "../../apps/$app/templates/pvc-$app-config.yaml"
-   kubectl -n $app -f "./populate-$app-config-pvc-job.yaml"
+   kubectl -n $app apply -f "../../apps/$app/templates/pvc-$app-config.yaml"
+   kubectl -n $app apply -f "./populate-$app-config-pvc-job.yaml"
 done
