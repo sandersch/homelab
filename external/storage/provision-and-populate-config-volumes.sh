c3edec716552f2149d144159ff44a1d09a0ad941
Charlie Sanders <sanderscharlie@gmail.com>
2021-12-15 23:26:56 -0600

    actually commit cmds in provisioning script

--- external/storage/provision-and-populate-config-volumes.sh
+++ external/storage/provision-and-populate-config-volumes.sh
@@ -5,6 +5,6 @@ set -euxo pipefail
 apps="sabnzbd sonarr radarr"
 
 for app in $apps; do
-  echo kubectl -n $app -f "../../apps/$app/templates/pvc-$app-config.yaml"
-  echo kubectl -n $app -f "./populate-$app-config-pvc-job.yaml"
+   kubectl -n $app -f "../../apps/$app/templates/pvc-$app-config.yaml"
+   kubectl -n $app -f "./populate-$app-config-pvc-job.yaml"
 done
