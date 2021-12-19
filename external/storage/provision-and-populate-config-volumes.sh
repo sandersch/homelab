ce1e768b323b45ae3e73d23887d8d59326761783
Charlie Sanders <sanderscharlie@gmail.com>
2021-12-18 19:43:46 -0600

    populate plex too

--- external/storage/provision-and-populate-config-volumes.sh
+++ external/storage/provision-and-populate-config-volumes.sh
@@ -2,9 +2,10 @@
 
 set -euxo pipefail
 
-apps="sabnzbd sonarr radarr"
+apps=plex "sabnzbd sonarr radarr"
 
 for app in $apps; do
-   kubectl -n $app apply -f "../../apps/$app/templates/pvc-$app-config.yaml"
-   kubectl -n $app apply -f "./populate-$app-config-pvc-job.yaml"
+  kubectl get namespace $app || kubectl create namespace $app
+  kubectl -n $app apply -f "../../apps/$app/templates/pvc-$app-config.yaml"
+  kubectl -n $app apply -f "./populate-$app-config-pvc-job.yaml"
 done
